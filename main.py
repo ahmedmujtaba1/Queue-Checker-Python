0b87fdbc3709e7dd3ae82e02e68c6d5c0bea37cb
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-11-01 06:39:43 +0500

    Setting up it

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import selenium, undetected_chromedriver as uc, time, csv
+import selenium, undetected_chromedriver as uc, time, csv, os, configparser
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from twocaptcha import TwoCaptcha
 
 chrome_options = uc.ChromeOptions()
 chrome_options.add_argument("--disable-dev-shm-usage")
@@ -16,6 +17,14 @@ driver = uc.Chrome(options=chrome_options, version_main=118)
 driver.get(url)
 time.sleep(2)
 wait = WebDriverWait(driver, 5)
+config = configparser.ConfigParser()
+config.read('setup.ini')
+two_captcha_api = config.get("admin","2captcha_api_key")
 captcha_image = wait.until(EC.presence_of_element_located((By.XPATH,"//img[@class='captcha-code']")))
 captcha_image.screenshot('captchas/captcha.png')
+solver = TwoCaptcha(apiKey=two_captcha_api)
+try:
+    result = solver.normal('captchas/captcha.png')
+except Exception as ex:
+    print("Error : ", ex)
 time.sleep(22222)
