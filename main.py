3fd8829a206b815876202f6ea74bb3ff7dc26537
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-11-01 20:33:23 +0500

    Checking

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ from twocaptcha import TwoCaptcha
 chrome_options = uc.ChromeOptions()
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-infobars')
 
 url = 'https://dfentertainment.queue-it.net/softblock/?c=dfentertainment&e=redhotconcertweek&cid=es-CL&rticr=0'
 driver = uc.Chrome(options=chrome_options, version_main=118)
+driver.maximize_window()
 driver.get(url)
 time.sleep(2)
 wait = WebDriverWait(driver, 5)
@@ -22,6 +23,7 @@ config.read('setup.ini')
 two_captcha_api = config.get("admin","2captcha_api_key")
 captcha_image = wait.until(EC.presence_of_element_located((By.XPATH,"//img[@class='captcha-code']")))
 captcha_image.screenshot('captchas/captcha.png')
+driver.find_element(By.ID,"playAudio").click()
 solver = TwoCaptcha(apiKey=two_captcha_api)
 try:
     result = solver.normal('captchas/captcha.png')
