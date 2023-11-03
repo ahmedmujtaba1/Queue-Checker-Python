d60b3df123f03be4c833755a44606c35ad9a0bf3
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-11-03 17:46:35 +0500

    Update check.py

--- check.py
+++ check.py
@@ -1,24 +1,30 @@
-import urllib.request
-import random
-username = 'themostpolenta'
-password = 'FiverPass1'
-country = 'DE'
-entry = "http://customer-themostpolenta-sessid-0457818374-sesstime-10:FiverPass1@pr.oxylabs.io:7777"
-query = urllib.request.ProxyHandler({
-    'http': entry,
-    'https': entry,
-})
-execute = urllib.request.build_opener(query)
-print(execute.open('https://ip.oxylabs.io').read())
-
+# import urllib.request
+# import random
 # from selenium import webdriver
 # from selenium.webdriver.common.by import By
 # import time
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument(f'--proxy-server=pr.oxylabs.io:7777')
-# driver = webdriver.Chrome(options=chrome_options)
-# #add argument with proxy infos
-# driver.get("https://customer-themostpolenta:FiverPass1@pr.oxylabs.io:7777")
-# driver.get('https://myexternalip.com/raw')
-# time.sleep(10000)
-# driver.quit()
+
+
+# username = 'themostpolenta'
+# password = 'FiverPass1'
+# country = 'DE'
+# entry = ('https://customer-%s-cc-%s:%s@pr.oxylabs.io:7777' %
+#     (username, country, password))
+# query = urllib.request.ProxyHandler({
+#     'http': entry,
+#     'https': entry,
+# })
+# execute = urllib.request.build_opener(query)
+# ip_address = execute.open(' https://ipinfo.io').read()
+# ip_address = str(ip_address).split(',')[0]
+# ip_address = str(ip_address).split('"')[3]
+# print("Ip Address : ", ip_address)
+
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument(f'--proxy-server={ip_address}:7777')
+# chrome_options.add_argument(f'--proxy-server={ip_address}:7777')
+driver = webdriver.Chrome(options=chrome_options)
+#add argument with proxy infos
+driver.get('https://myexternalip.com/raw')
+time.sleep(10000)
+driver.quit()
