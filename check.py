51471d6ea977ccaf0ad0227c1c176d5017b76242
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-11-03 06:30:38 +0500

    Update check.py

--- check.py
+++ check.py
@@ -1,56 +1,16 @@
-# import re
-# from typing import Optional
-
-# from seleniumwire import webdriver
-# from selenium.webdriver.chrome.service import Service
-# # A package to have a chromedriver always up-to-date.
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# USERNAME = ""
-# PASSWORD = ""
-# ENDPOINT = "pr.oxylabs.io:7777"
-
-
-# def chrome_proxy(user: str, password: str, endpoint: str) -> dict:
-#     wire_options = {
-#         "proxy": {
-#             "http": f"http://{user}:{password}@{endpoint}",
-#             "https": f"https://customer-themostpolenta-sessid-0637001499-sesstime-10:FiverPass2@pr.oxylabs.io:7777",
-#         }
-#     }
-
-#     return wire_options
-
-
-# def get_ip_via_chrome():
-#     coptions = webdriver.ChromeOptions()
-#     coptions.headless = True
-#     proxies = chrome_proxy(USERNAME, PASSWORD, ENDPOINT)
-#     driver = webdriver.Chrome(ChromeDriverManager().install(), options=coptions, seleniumwire_options=proxies)
-#     try:
-#         driver.get("https://ip.oxylobs.io/")
-#         return driver.page_source
-#     finally:
-#         driver.quit()
-
-# print(get_ip_via_chrome())
-
-
-# if __name__ == "__main__":
-#     print(get_ip_via_chrome())
+# import urllib.request
+# import random
+# username = 'themostpolenta'
+# password = 'FiverPass1'
+# country = 'DE'
+# entry = ('http://customer-%s-cc-%s:%s@pr.oxylabs.io:7777' %
+#     (username, country, password))
+# query = urllib.request.ProxyHandler({
+#     'http': entry,
+#     'https': entry,
+# })
+# execute = urllib.request.build_opener(query)
+# print(execute.open('https://ip.oxylabs.io').read())
 
 from selenium import webdriver
-import time
-
-
-chrome_options = webdriver.ChromeOptions()
-file_path = r"B:\Ahmed'sCode\Fiverr\0rder 6\proxy"
-chrome_options.add_argument(f'--load-extension={file_path}')
-# chrome_options.add_argument(f'--proxy-server={proxy_ip_port}')
-driver = webdriver.Chrome(options=chrome_options)
-
-driver.get('http://whatismyipaddress.com')
-
-time.sleep(8)
-
-driver.quit()
+from selenium.webdriver.common.by import By
