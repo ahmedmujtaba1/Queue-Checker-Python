06fa3f887220f3c77c81a173f824bdd497c066a7
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-11-05 06:47:47 +0500

    Test

--- i.py
+++
@@ -1,5 +0,0 @@
-import pyautogui
-button_location = pyautogui.locateOnScreen('img/button.png')
-button_exact_location = pyautogui.center(button_location)
-print(button_exact_location)
-pyautogui.moveTo(button_exact_location)
